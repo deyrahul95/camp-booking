a0e25dd7ebf705cd791bd6d901d3104885451022
Rahul Dey <rahuldey9623@gmail.com>
2023-11-21 21:19:52 +0530

    Upgrade to dotnet 8

--- Backend/CampBooking.Domain/DTOs/LoginUser.cs
+++ Backend/CampBooking.Domain/DTOs/LoginUser.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CampBooking.Domain.DTOs
+﻿namespace CampBooking.Domain.DTOs
 {
     public class LoginUser
     {
